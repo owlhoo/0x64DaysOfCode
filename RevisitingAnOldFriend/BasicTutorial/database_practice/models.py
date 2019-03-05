72b8445a8afbef26d7c397f72358c28a454e2112
owlhoo <anes1996_h@hotmail.com>
2019-03-05 23:20:23 +0100

    Day 4 - done!

--- RevisitingAnOldFriend/BasicTutorial/database_practice/models.py
+++ RevisitingAnOldFriend/BasicTutorial/database_practice/models.py
@@ -3,36 +3,52 @@ from django.db import models
 # Create your models here.
 
 
-class Musician (models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    surname = models.CharField(max_length=50, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Musician (CommonInfo):
     instrument = models.CharField(max_length=50)
 
 
-class Album(models.Model):
+class Album(CommonInfo):
     artist = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
     release_date = models.DateField()
     num_stars = models.IntegerField()
 
 
-class Person(models.Model):
+class Person(CommonInfo):
     SHIRT_SIZES = (
         ('S', 'SMALL'),
         ('M', 'MEDIUM'),
         ('L', 'LARGE'),
     )
-    name = models.CharField(max_length=50)
     shirt_size = models.CharField(max_length=6, choices=SHIRT_SIZES)
 # get_shirt_size_display() will show full length choice
 # while the S, M or L will be saved in the database
+
+    @property
+    def person_name(self):
+        """Returns name of the person :D"""
+        return f"{str(self.name)} {str(self.shirt_size)}"
+
+    @person_name.setter
+    def person_name(self, name):
+        self.name = name
+
+    @person_name.deleter
+    def person_name(self):
+        del self.name
 
     def __str__(self):
         return self.name
 
 
-class Group(models.Model):
-    name = models.CharField(max_length=50)
+class Group(CommonInfo):
     members = models.ManyToManyField(Person, through='Membership')
 
     def __str__(self):
@@ -46,9 +62,8 @@ class Membership(models.Model):
     invite_reason = models.CharField(max_length=256)
 
 
-class Man(models.Model):
+class Man(CommonInfo):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    surname = models.CharField(max_length=50)
 
     def __str__(self):
         return self.person.name
@@ -56,4 +71,13 @@ class Man(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         print("Done saving..", end="hehe\n")
-        self.__str__()
+        return self.__str__()
+
+
+class Room(models.Model):
+    wall_color = models.CharField(max_length=50)
+    volume = models.IntegerField(default=2)
+
+
+class Corridor(Room):
+    has_furniture = models.BooleanField(default=False)
