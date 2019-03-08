43e4d9c347971a0943a59c5c0194d78e3a0b8b1f
owlhoo <anes1996_h@hotmail.com>
2019-03-08 01:07:58 +0100

    Day 6 - done!

--- RevisitingAnOldFriend/BasicTutorial/class_stuff.py
+++ RevisitingAnOldFriend/BasicTutorial/class_stuff.py
@@ -2,53 +2,50 @@
 
 
 class A:
-	def __init__(self, value):
-		self.value = value
+    def __init__(self, value):
+        self.value = value
 
-	def __str__(self):
-		return print("My value is " + str(self.value))
+    def __str__(self):
+        return print("My value is " + str(self.value))
 
-	@property
-	def who(self):
-		print("HELLO I AM RETURNING A VALUE")
-		return print(str(self.value))
+    @property
 
-	@who.setter
-	def who(self, value):
-		self.value = value
+    def who(self):
+        print("HELLO I AM RETURNING A VALUE")
+        return print(str(self.value))
 
-	@who.deleter
-	def who(self):
-		del self.value
+    @who.setter
+    def who(self, value):
+        self.value = value
+
+    @who.deleter
+    def who(self):
+        del self.value
 
 
 class B:
-	def __init__(self, value=15):
-		self.value = value
+    def __init__(self, value=15):
+        self.value = value
 
-	def __str__(self):
-		return str(self.value)
+    def __str__(self):
+        return str(self.value)
 
-	@classmethod
-	def create_element(cls, value):
-		return cls(value + 25)
+    @classmethod
+    def create_element(cls, value):
+        return cls(value + 25)
 
 
 def main():
-	a1 = A(5)
-	a1.who
-	a1.who = 35
-	a1.who
-	del a1
+    a1 = A(5)
+    a1.who
+    a1.who = 35
+    a1.who
+    del a1
 
-	b1 = B()
-	b2 = B.create_element(25)
-	print(b1, b2)
+    b1 = B()
+    b2 = B.create_element(25)
+    print(b1, b2)
 
 
 if __name__ == '__main__':
-	main()
-
-
-
-
+    main()
--- RevisitingAnOldFriend/BasicTutorial/polls/views.py
+++ RevisitingAnOldFriend/BasicTutorial/polls/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
-from .models import Question
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.db.models import F
+from django.urls import reverse
+
+from .models import Question, Choice
+
 # from django.http import Http404
 # from django.template import loader
 
@@ -42,9 +46,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+        return render(request, 'polls/results.html', {'question': question})
+    except Question.DoesNotExist:
+        return render(request, 'polls/results.html', {'error_message': "Requested question does not exist, sorry. :("})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the voting form
+        return render(request, 'polls/detail.html', {'question': question,
+                                                     'error_message': "You didn't select a choice.", })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
+
+
