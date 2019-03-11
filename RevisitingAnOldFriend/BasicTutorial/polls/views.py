dce2853f7d92968b81ed754d65c5eac2703f8756
owlhoo <anes1996_h@hotmail.com>
2019-03-11 01:41:07 +0100

    Day 9 - done!

--- RevisitingAnOldFriend/BasicTutorial/polls/views.py
+++ RevisitingAnOldFriend/BasicTutorial/polls/views.py
@@ -78,7 +78,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
