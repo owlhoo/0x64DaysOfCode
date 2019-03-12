7d4b1a6391169409834a1c0638388cba7b39671f
owlhoo <anes1996_h@hotmail.com>
2019-03-12 01:21:05 +0100

    Day 10 - done!

--- RevisitingAnOldFriend/BasicTutorial/polls/admin.py
+++ RevisitingAnOldFriend/BasicTutorial/polls/admin.py
@@ -5,8 +5,34 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-# Register your models here.
 
-admin.site.register(Question)
+# class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Question',         {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+# Choice in line replaces this I guess xd
+# class ChoiceAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         ('Choice',          {'fields': ['choice_text']}),
+#         ('Votes',           {'fields': ['votes']}),
+#         ('Question source', {'fields': ['question']})
+#     ]
+# admin.site.register(Choice, ChoiceAdmin)
+
+
+admin.site.register(Question, QuestionAdmin)
+
