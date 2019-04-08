ca0c63b2185d8319d75c419b246beb06f6bb95f7
owlhoo <anes1996_h@hotmail.com>
2019-04-08 02:20:03 +0200

    Day 37 - done!

---
+++ Meet my pyppy/net/UDP/real_face_udp.py
@@ -0,0 +1,60 @@
+import argparse
+import random
+import socket
+import sys
+
+MAX_BYTES = 65535
+
+
+def server(interface, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((interface, port))
+    print(f'Listening at {sock.getsockname()}')
+
+    while True:
+        data, address = sock.recvfrom(MAX_BYTES)
+        if random.random() < 0.5:
+            print(f'Pretending to drop a packet from {address}')
+            continue
+        text = data.decode('ascii')
+        print(f'The client at {address} says {text}')
+        message = f'Your data was {len(data)} byes long'
+        sock.sendto(message.encode('ascii'), address)
+
+
+def client(hostname, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    hostname = sys.argv[2]
+    sock.connect((hostname, port))
+    print(f'Client socket name is {sock.getsockname()}')
+
+    delay = 0.1  # secs
+    text = 'This is another message'
+    data = text.encode('ascii')
+    while True:
+        sock.send(data)
+        print(f'Waiting up to {delay} seconds for a reply')
+        sock.settimeout(delay)
+        try:
+            data = sock.recvfrom(MAX_BYTES)
+        except socket.timeout:
+            delay *= 2  # wait even longer for the next request
+            if delay > 2.0:
+                raise RuntimeError('I think the server is down')
+            else:
+                break  # we are done, looping can be stopped
+
+        print(f'The server says {data.decode("ascii")}')
+
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    parser = argparse.ArgumentParser(description='Send and receive UDP,'
+                                                 ' pretending packets are often dropped')
+    parser.add_argument('role', choices=choices, help='which role to take')
+    parser.add_argument('host', help='interface the servers listens at;'
+                                     ' host the client sends to')
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060, help='UDP port (default 1060)')
+    args = parser.parse_args()
+    function = choices[args.role]
+    function(args.host, args.p)
