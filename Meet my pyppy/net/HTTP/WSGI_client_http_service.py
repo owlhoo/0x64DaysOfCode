a3933fb8dc631cfd99c36f1c6498350e91dadf06
owlhoo <anes1996_h@hotmail.com>
2019-04-29 02:04:54 +0200

    Day 53 - done!

---
+++ Meet my pyppy/net/HTTP/WSGI_client_http_service.py
@@ -0,0 +1,16 @@
+from pprint import pformat
+from wsgiref.simple_server import make_server
+
+
+def app(environ, start_response):
+    headers = {'Content-Type': 'text/plain; charset=utf-8'}
+    start_response('200 OKCAOPOY', list(headers.items()))
+    yield "Here is the WSGI environment:\r\n\r\n".encode('utf-8')
+    yield pformat(environ).encode('utf-8')
+
+
+if __name__ == '__main__':
+    httpd = make_server('localhost', 8000, app)
+    host, port = httpd.socket.getsockname()
+    print(f'Serving on {host} port {port}')
+    httpd.serve_forever()
